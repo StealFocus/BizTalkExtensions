6e48e5d9d77c0deaefd29d71f1cc76e696c8856b
Callum Hibbert <callum.hibbert@gmail.com>
2013-02-08 22:47:56 +0000

    First cut of solution.

---
+++ Source/StealFocus.BizTalkExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("StealFocus.BizTalkExtensions")]
+[assembly: AssemblyTitle("StealFocus.BizTalkExtensions")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("302be64d-fd4c-4050-99d0-a0513a0c397b")]
+[assembly: NeutralResourcesLanguage("en-GB")]
+[assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")] // Assembly is delay signed.
+[assembly: SuppressMessage("Microsoft.Design", "CA1016:MarkAssembliesWithAssemblyVersion")] // Development version is "0.0.0.0"
+[assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")] // "AssemblyInformationalVersion" is a string
+
+[assembly: InternalsVisibleTo("StealFocus.BizTalkExtensions.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009b6924a804e9b7418d1e5d0989fc0a1e8bb2ab17cd81114a1daa048f167f1779f791ef55045ff77490e17f511e8156e5980a46b64e83b64d95bf4fe5a05e7b0f3db9c5651a0edfcedfaa1f83dc3527bf870043270ac6a72a0e1e6db84a1f8e31a9dc605bb11fef8109e2a558cb78a711f5a4317898c5913ea8701dba971390e6")]
